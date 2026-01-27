ea6173322f038451f2e05bc6d653ceb0ff67d16d
arthurfreitasys <arthur.freitasys@gmail.com>
2026-01-27 15:54:21 -0300

    style: Estilização da rolagem do site  com a biblioteca 'ScrollReveal'
    Usando a biblioteca de scroll reveal, estilizei a rolagem do site

---
+++ reveal.js
@@ -0,0 +1,58 @@
+window.revelar = ScrollReveal({reset:true})
+
+revelar.reveal('.efeito-texto-inicio',{
+    duration: 1500,
+    distance: '90px',
+    origin: 'left'
+})
+
+revelar.reveal('.efeito-img-inicio',{
+    duration: 1500,
+    distance: '90px',
+    origin: 'right'
+})
+
+
+revelar.reveal('.conteudo-sobre img', {
+    duration: 2000,
+    distance: '90px',
+    origin: 'left'
+})
+
+revelar.reveal('.efeito-txt-sobre', {
+    duration: 2000,
+    distance: '50px',
+    origin: 'right'
+})
+
+revelar.reveal('.efeito-txt-habilidades', {
+    duration: 2000,
+    distance: '50px',
+    origin: 'top'
+})
+
+revelar.reveal('.efeito-icones-habilidades', {
+    duration: 2000,
+    delay: '200',
+    distance: '50px',
+    origin: 'bottom'
+})
+
+revelar.reveal('.efeito-txt-projetos', {
+    duration: 2000,
+    distance: '50px',
+    origin: 'top',
+    delay: 200
+})
+
+revelar.reveal('.card1', {
+    duration: 2000,
+    distance: '50px',
+    origin: 'left',
+})
+
+revelar.reveal('.card2', {
+    duration: 2500,
+    distance: '50px',
+    origin: 'right'
+})
